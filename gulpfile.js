899546b8635ad76c9f6e92697b33756c272c6a50
Sergey Chernov <scout1982@mail.ru>
2016-10-07 23:24:18 +0300

    auto

--- gulpfile.js
+++ gulpfile.js
@@ -6,32 +6,64 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
+
 
 gulp.task("style", function() {
-  gulp.src("sass/style.scss")
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(postcss([
-      autoprefixer({browsers: [
-        "last 1 version",
-        "last 2 Chrome versions",
-        "last 2 Firefox versions",
-        "last 2 Opera versions",
-        "last 2 Edge versions"
-      ]})
-    ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+	gulp.src("sass/style.scss")
+	.pipe(plumber())
+	.pipe(sass())
+	.pipe(postcss([
+		autoprefixer({browsers: [
+			"last 1 version",
+			"last 2 Chrome versions",
+			"last 2 Firefox versions",
+			"last 2 Opera versions",
+			"last 2 Edge versions"
+			]}),
+		mqpacker({
+			sort: true
+		})
+	]))
+	.pipe(gulp.dest("css"))
+	.pipe(minify())
+	.pipe(rename("style.min.css"))
+	.pipe(gulp.dest("css"))
+	.pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
-  server.init({
-    server: ".",
-    notify: false,
-    open: true,
-    ui: false
-  });
+gulp.task("images", function() {
+	return gulp.src("*img/**/*.{png,jpg.gif}")
+	.pipe(imagemin([
+		imagemin.optipng({optimizationlevel: 3}),
+		imagemin.jpegtran({progressive: true})
+	]))
+	.pipe(gulp.dest("img"));
+})
 
-  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+gulp.task("symbols", function() {
+	return gulp.src("*img/svgsprite/*.svg")
+	.pipe(svgmin())
+	.pipe(svgstore({
+		inlinesvg: true
+	}))
+	.pipe(rename("symbols.svg"))
+	.pipe(gulp.dest("img"));
+})
+
+gulp.task("serve", ["style"], function() {
+	server.init({
+		server: ".",
+		notify: false,
+		open: true,
+		ui: false
+	});
+
+	gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+	gulp.watch("*.html").on("change", server.reload);
 });
